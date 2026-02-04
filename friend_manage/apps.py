232ce8f52dff0ad42bc7c9a4650276432c5d8a32
riyatamang001 <tamangriya66778@gmail.com>
2026-02-04 20:32:49 +0545

    Made the app for each functionalities/features : different apps = different responsibilities.

---
+++ friend_manage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FriendManageConfig(AppConfig):
+    name = 'friend_manage'
---
+++ protfolio/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+# Create your views here.
